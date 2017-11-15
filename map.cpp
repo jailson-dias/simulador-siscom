d5d16629dc6b8277f540d0242b6b5e1dd5620db2
Jailson Dias <jcd2@cin.ufpe.br>
2017-11-15 00:22:40 -0300

    modificações no MS-MAP(ainda sem funcionar)

--- map.cpp
+++ map.cpp
@@ -90,21 +90,22 @@ vector<vector<int> > table2(double frame) {
 
 
 double valor(double frame, double success, double colisao) {
+
     double maxi = 0;
     double n = -1;
     double s = 0, c = 0;
     double empty = frame - colisao - success;
     // cout << "empty: " << empty << " colisao: " << colisao << " success: " << success << endl;
-    for (int i = 2; i<100;i++) {
+    for (int i = 2; i<=64;i++) {
         double a1 = (fat(frame)/(fat(empty)*fat(success)*fat(colisao)));
-        cout << a1 << endl;
+        // cout << a1 << endl;
         double a2 = p1(frame, empty, i);
         double a3 = p2(frame, empty, success, i);
         double a4 = p3(success, colisao, i);
-        cout << "p1: " << a2 << " p2: " << a3 << " p3: " << a4 << endl;
+        // cout << "p1: " << a2 << " p2: " << a3 << " p3: " << a4 << endl;
         double p = a1*a2*a3*a4;
 
-        cout << p << "\t==>\t" << i << endl;
+        // cout << p << "\t==>\t" << i << endl;
 
 
         if (isfinite(p) && p < 1 && p>maxi) {
@@ -122,9 +123,9 @@ double valor(double frame, double success, double colisao) {
 int table1(double n) {
     double U = 0;
     int f = -1;
-    for(int i = 2;i<=2000;i=i*2) {
+    for(int i = 2;i<=10000;i=i*2) {
         double auxU = (n/((double)i))*pow(1-(1.0/i),n - 1);
-        // cout << auxU << " " << U << endl;
+        cout << auxU << " " << U << endl;
         if(auxU - U > 0.00000001) {
             U = auxU;
             f = i;
@@ -154,78 +155,88 @@ int fsub(int n) {
 }
 
 int main(int argc, char **argv){
-    // table(10);
-    // while(1) {
-    //     int x;
-    //     cin >> x; 
-    //     cout << table1(x) << endl;
-    // }
-    vector<vector<int> > t = table2(50);
-    // for(int i = 0; i < t.size();i++) {
-    //     for (int j = 0; j < t[i].size(); j++) {
-    //         cout << t[i][j] << "\t";
-    //     }
-    //     cout << endl;
-    // }
-    int quanttags = 100;
-    int Nback = 64;
+
+    // cout << valor(10, 0, 8) << endl;
+
+    int quanttags = 1000;
+    int Nback = 64, Fnxt = 64;
     int K = -1;
     while (quanttags > 0) {
-        int slots[Nback],
+        int slots[Fnxt],
             sucesso = 0, // quantidade de slots que leram as etiquetas com sucesso
             colisao = 0, // quantidade de slots que tiveram colisao
             vazio = 0, // quantidade de slots vazios
             S = 0; // Sucesso total
 
-        for (int i = 0; i< Nback;i++) {
+        for (int i = 0; i< Fnxt;i++) {
             slots[i] = 0; // coloca 0 em todos os slots
         }
 
         for (int i = 0; i < quanttags; i++) {
-            slots[rand() % Nback] += 1; // gera um numero aleatorio entre 0 e a quantidade de slots e no slot do numero que foi gerado acrescenta um nele
-        }
-        int Fsub = fsub(Nback);
-        if (K == 1)
-            Fsub = Nback;
-        for (int i = 0; i< Fsub;i++) {
-            if (slots[i] == 0) {
-                vazio++; // pega a quantidade de slots vazios
-            } else if (slots[i] == 1) {
-                sucesso++; // pega a quantidade de slots que conseguiram reconhecer tags
-                S++;
-            } else {
-                colisao++; // pega a quantidade de slots que teve colisao
-            }
+            slots[rand() % Fnxt] += 1; // gera um numero aleatorio entre 0 e a quantidade de slots e no slot do numero que foi gerado acrescenta um nele
         }
 
-        int n = t[colisao][sucesso];
-        K = ceil(Nback/Fsub);
+        int Fsub = fsub(Fnxt);
+        if (K == 1)
+            Fsub = Fnxt;
+        int Ns, Nc;
+        do {
+            Ns=0, Nc=0;
+            for (int i = 0; i< Fsub;i++) {
+                if (slots[i] == 1) {
+                    Ns++;
+                } else if (slots[i] > 1) {
+                    Nc++;
+                }
+            }
+            if (Nc == 0 && Ns == 0) {
+                if (K == 1) {
+                    break;
+                }
+                Fsub *= 2;
+                if (K!=-1) {
+                    K =  (K + 1)/2;
+                }
+            }
+        } while((Nc == 0 && Ns == 0) || K > 1);
+        cout << "Ns: " << Ns << ", Nc: " << Nc << ", Fsub: " << Fsub << ", ";
+        int n = valor(Fsub,Ns,Nc);
+        K = ceil(Fnxt/Fsub);
         int Nest = n * K;
         int nslots = table1(Nest);
-        if (Nback <= nslots && Nback >= nslots) {
-            Nest = 0;
+        // cout << Nest << "  " << nslots << "  " << Fnxt << endl;
+        cout << "n: " << n << ", K: " << K << ", Nest: " << Nest << ", nslots: " << nslots << ", Fnxt: " << Fnxt << ", ";
+        if (Fnxt <= nslots && Fnxt >= nslots/2) {
+            Nest = S = 0;
+            cout << endl;
             for (int i = 0;i<K;i++) {
-                vazio = sucesso = colisao = 0;
-                for (int j = i*Fsub; j< min((i+1)*Fsub, Nback);j++) {
-                    if (slots[j] == 0) {
-                        vazio++; // pega a quantidade de slots vazios
-                    } else if (slots[j] == 1) {
-                        sucesso++; // pega a quantidade de slots que conseguiram reconhecer tags
+                Ns = Nc = 0;
+                int lenframe = min((i+1)*Fsub, Fnxt);
+                // cout << "len: " << lenframe << " ";
+                for (int j = i*Fsub; j< lenframe;j++) {
+                    if (slots[j] == 1) {
+                        Ns++;
                         S++;
-                    } else {
-                        colisao++; // pega a quantidade de slots que teve colisao
+                    } else if (slots[j] > 1) {
+                        Nc++;
                     }
                 }
-                Nest += t[colisao][sucesso];
+                // cout << "Nc: " << Nc << ", Ns: " << Ns << ", vv: " << valor(lenframe - i*Fsub, Ns, Nc) << endl;
+                Nest += valor(lenframe, Ns, Nc);
             }
             Nback = Nest - S;
+            quanttags -= S;
+            Fnxt = table1(Nback);
             K = 1;
+            cout << "Nest: " << Nest << ", S: " << S << ", Nback: " << Nback << endl << endl;
         } else {
-            Nback = Nest - sucesso;
+            Nback = Nest - Ns;
+            quanttags -= Ns*K;
+            Fnxt = table1(Nback*K);
             K = 0;
         }
-        cout << "back: " << Nback << endl;
-        quanttags -= S;
+        cout << "Fnxt: " << Fnxt << ", Tags: " << quanttags << endl;
+        if (Fnxt == -1) break;
     }
 	return 0;
 }
